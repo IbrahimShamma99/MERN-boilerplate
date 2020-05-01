ed4346d9f28037bb16edf42f5ee01951a8192191
ibrahimshamma99 <i.abushammah@gmail.com>
2020-05-01 16:32:37 +0300

    create branch

---
+++ client/src/store/actions.js
@@ -0,0 +1,10 @@
+const LOGIN = "USER_LOGIN";
+const UPDATE = "USER_UPDATE";
+const LOGOUT = "USER_LOGOUT";
+const REGISTER = "USER_REGISTER";
+
+const actionTypes = {
+    LOGIN,UPDATE,
+    LOGOUT,REGISTER
+};
+export default actionTypes;
