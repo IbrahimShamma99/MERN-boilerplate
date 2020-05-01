22022a8fec27d5a13dc4f9c0d57c201c7b59a6a4
ibrahimshamma99 <i.abushammah@gmail.com>
2020-05-01 16:42:09 +0300

    update init state

--- client/src/store/actions.js
+++ client/src/store/actions.js
@@ -2,9 +2,3 @@ export const LOGIN = "USER_LOGIN";
 export const UPDATE = "USER_UPDATE";
 export const LOGOUT = "USER_LOGOUT";
 export const REGISTER = "USER_REGISTER";
-
-// const actionTypes = {
-//     LOGIN,UPDATE,
-//     LOGOUT,REGISTER
-// };
-// export default actionTypes;
--- client/src/store/reducers.js
+++ client/src/store/reducers.js
@@ -1,6 +1,32 @@
 import * as actionTypes from './actions';
-
-const intialState = {};
+import propTypes from 'prop-types';
+const intialState = {
+    first_name:propTypes.string,
+    last_name:propTypes.string,
+    bio:propTypes.string,
+    contacts:[{
+        link:propTypes.string,
+        platform:propTypes.string
+    },{
+        link:propTypes.string,
+        platform:propTypes.string
+    },{
+        link:propTypes.string,
+        platform:propTypes.string
+    },
+    {
+        link:propTypes.string,
+        platform:propTypes.string
+    },
+    {
+        link:propTypes.string,
+        platform:propTypes.string
+    }],
+    collections:[{}],
+    interests:[],
+    email:propTypes.string,
+    location:propTypes.string
+};
 
 const reducers = (state=intialState,actions)=>{
 
