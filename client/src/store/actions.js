621b1dc62bfaf6623fa09b6cbbbaa9401b09bc1e
ibrahimshamma99 <i.abushammah@gmail.com>
2020-05-09 01:43:14 +0300

    update

--- client/src/store/actions.js
+++ client/src/store/actions.js
@@ -1,3 +1,4 @@
+export const INIT = () => "INIT_STATE";
 export const MODIFY = "USER_EDIT";
 export const LOGIN = ()=> "USER_LOGIN";
 export const UPDATE = () => "USER_UPDATE";
@@ -5,4 +6,3 @@ export const LOGOUT = () => "USER_LOGOUT";
 export const REGISTER = () => "USER_REGISTER";
 export const SUCCESS = () => "SUCCESS";
 export const ERROR = () => "ERROR";
-export const INIT = () => "INIT_STATE";
