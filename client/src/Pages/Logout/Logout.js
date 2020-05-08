1f55a1e6a92bf08b275a0ecb499cbae0ac804b7f
ibrahimshamma99 <i.abushammah@gmail.com>
2020-05-09 01:17:24 +0300

    ujpdate

--- client/src/Pages/Logout/Logout.js
+++ client/src/Pages/Logout/Logout.js
@@ -1,26 +1,25 @@
-import React from 'react';
+import React from "react";
 import { connect } from "react-redux";
 import * as actionTypes from "../../store/actions";
 
 const logout = (props) => (
-    <div>
-        <p>loging out</p>
-    </div>
+  <div>
+    <p>loging out</p>
+  </div>
 );
 
 const mapStateToProps = (state) => {
-    const LogoutState = {};
-    return LogoutState;
+  const LogoutState = {
+    open: state.open,
   };
-  
-  const mapDispatchToProps = (dispatch) => {
-    return {
-      change: (name, value) =>
-        dispatch({ type: actionTypes.MODIFY, name, value }),
-      submit: () => dispatch({ type: actionTypes.REGISTER }),
-    };
-  };
-  
+  return LogoutState;
+};
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    change: (name, value) =>
+      dispatch({ type: actionTypes.MODIFY, name, value }),
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(logout);
