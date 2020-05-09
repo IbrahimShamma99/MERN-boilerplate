7404b977ca91d51274113f285e4d60e474943772
ibrahimshamma99 <i.abushammah@gmail.com>
2020-05-10 02:16:59 +0300

    update

---
+++ ServerSide/helper/upload.js
@@ -0,0 +1,17 @@
+const multer = require("multer");
+var path = require("path");
+
+var AvatarStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    console.log(path.join(__dirname, "../public/avatar/"));
+    cb(null, path.join(__dirname, "../public/avatar/"));
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().getTime() + "-" + file.originalname);
+  },
+});
+
+var uploadAvatar = multer({
+  storage: AvatarStorage,
+  limits: { fileSize: 1024 * 1024 * 5 },
+});
