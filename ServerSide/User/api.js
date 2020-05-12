6da40811dcf6f1934d31bf24561ddc508d8e79a2
ibrahimshamma99 <i.abushammah@gmail.com>
2020-05-12 06:14:18 +0300

    update

--- ServerSide/User/api.js
+++ ServerSide/User/api.js
@@ -1,21 +1,30 @@
-var router = require('express').Router();
-const controlers = require('./controlers');
+const router = require("express").Router();
+const controlers = require("./controlers");
 const Routes = require("./constants");
 const helper = require("../helper/helper");
-const {uploadAvatar} = require("../helper/");
-//SECTION add user
-router.post(Routes.AddUser,controlers.adduser);
+const { uploadAvatar } = require("../helper/");
+const userParams = require("./params");
 
-//SECTION user login 
+router.param("user", userParams);
+
+//SECTION add user
+router.post(Routes.AddUser, controlers.adduser);
+
+//SECTION user login
 router.post(Routes.Login, controlers.login);
 
 //SECTION update user
-router.put(Routes.update , helper.required , uploadAvatar.single("avatar"),controlers.updateUser);
+router.put(
+  Routes.update,
+  helper.required,
+  uploadAvatar.single("avatar"),
+  controlers.updateUser
+);
 
 //SECTION follow user
-router.post(Routes.follow , helper.required , controlers.followUser);
+router.post(Routes.follow, helper.required, controlers.followUser);
 
 //SECTION logout user
-router.post(Routes.Logout , helper.optional , controlers.logout);
+router.post(Routes.Logout, helper.optional, controlers.logout);
 
 module.exports = router;
