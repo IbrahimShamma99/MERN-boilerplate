117d783be711206bc7c80f154fd70db1cc537c84
ibrahimshamma99 <i.abushammah@gmail.com>
2020-05-15 16:04:19 +0300

    update

--- client/src/Switcher.js
+++ client/src/Switcher.js
@@ -1,31 +1,32 @@
-import React from 'react';
-import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import RouteNames from "./constants/routes";
 
 //SECTION Pages
 import Login from "./Pages/Login/";
 import Signup from "./Pages/Register/";
 import Home from "./Pages/Home";
-import Profile from './Pages/Profile';
-import Update from './Pages/Update';
+import Profile from "./Pages/Profile";
+import Update from "./Pages/Update";
 import NavigationBar from "./Navigation/Naviagation";
 
-const switcher = (props) => {
-  return (
-    <div>
-    <NavigationBar/>
+class switcher extends React.Component {
+  render() {
+    return (
+      <div>
+        <NavigationBar />
 
-      <Router>
-        <Switch>
-          <Route path={RouteNames.home} component={Home}/>
-          <Route path={RouteNames.login} component = {Login}/>
-          <Route path={RouteNames.register} component={Signup}/>
-          <Route path={RouteNames.profile} component={Profile}/>
-          <Route path={RouteNames.update} component={Update}/>
-        </Switch>
-      </Router>
-    </div>
-  );
+        <Router>
+          <Switch>
+            <Route path={RouteNames.home} component={Home} />
+            <Route path={RouteNames.login} component={Login} />
+            <Route path={RouteNames.register} component={Signup} />
+            <Route path={RouteNames.profile} component={Profile} />
+            <Route path={RouteNames.update} component={Update} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
-
 export default switcher;
