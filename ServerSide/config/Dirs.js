19709870140d6ab87a919cd91bdc2e32559627cf
ibrahimshamma99 <i.abushammah@gmail.com>
2020-05-21 21:45:54 +0300

    👌 IMPROVE:

--- ServerSide/config/Dirs.js
+++ ServerSide/config/Dirs.js
@@ -1,22 +1,10 @@
 const path = require("path");
-const fs = require("fs");
 const PublicDir = path.join(__dirname, "../public/");
 const AvatarDir = path.join(__dirname, "../public/avatar/");
 console.log("\u{1F9F1} CWD Backend \u{1F9F1} ", __dirname);
 console.log("\u{1F9F1} CWD exported static \u{1F9F1} ", AvatarDir);
 
-const CreateDir = (Dir) => {
-  if (!fs.existsSync(Dir)) {
-    fs.mkdirSync(Dir);
-  }
-};
-
+//SECTION Add here exported Dirs for you client side app
 const Dirs = [PublicDir, AvatarDir];
 
-const CreateDirs = () => {
-  Dirs.map((dir) => {
-    CreateDir(dir);
-  });
-};
-
-module.exports = CreateDirs;
+module.exports = Dirs;
--- ServerSide/server/app.js
+++ ServerSide/server/app.js
@@ -1,6 +1,6 @@
 const app = require("./server");
 const { PORT } = require("../config/config.json");
-const CreateDirs = require("../config/Dirs");
+const CreateDirs = require("../config/CreateDirs");
 
 //SECTION The APP is listenning on 5000 Port
 var server = app.listen(process.env.PORT || PORT, function () {
