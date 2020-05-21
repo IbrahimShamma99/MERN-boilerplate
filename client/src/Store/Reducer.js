274f18a724ad10a25d7ad8d77b180a915ea0b35a
ibrahimshamma99 <i.abushammah@gmail.com>
2020-05-21 04:35:51 +0300

    🐛 FIX:

--- client/src/Store/Reducer.js
+++ client/src/Store/Reducer.js
@@ -1,18 +1,11 @@
 import userReducer from "../USER/Store/user.reducers";
 import utileReducer from "../UtilStore/util.reducers";
-import storage from "redux-persist/lib/storage";
 import { combineReducers } from "redux";
 import { persistReducer } from "redux-persist";
-
-const persistConfig = {
-  user: {
-    key: "user",
-    storage: storage,
-  },
-};
+import persistConfig from './Config';
 
 const rootReducer = combineReducers({
-  util: utileReducer,
+  util: persistReducer(persistConfig.util,utileReducer),
   UserState: persistReducer(persistConfig.user, userReducer),
 });
 
--- client/src/Store/store.js
+++ client/src/Store/store.js
@@ -1,7 +1,7 @@
 import { createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 import { persistStore } from "redux-persist";
-import rootReducer from './ConfigReducer';
+import rootReducer from './Reducer';
 import getMiddleware from './Middlewares';
 
 const store = createStore(rootReducer, composeWithDevTools(getMiddleware()));
