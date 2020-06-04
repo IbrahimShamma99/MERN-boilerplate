9e5202fd46a047713160e5329e175229bbae9ca3
ibrahimshamma99 <i.abushammah@gmail.com>
2020-06-04 20:17:56 +0300

    👌 IMPROVE:

--- client/src/Store/store.js
+++ client/src/Store/store.js
@@ -1,9 +1,35 @@
 import { createStore } from "redux";
 import { persistStore } from "redux-persist";
 import rootReducer from "./Reducer";
-import Middleware from "./Middleware/";
+import thunkMiddleware from "redux-thunk";
+import { createLogger } from "redux-logger";
+import { applyMiddleware } from "redux";
+import asyncDispatchMiddleware from "async-dispatch";
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 
-const store = createStore(rootReducer, Middleware);
+const loggerMiddleware = createLogger();
+
+const DevMiddlewares = applyMiddleware(
+  thunkMiddleware,
+  asyncDispatchMiddleware
+);
+const ProdMiddlewares = applyMiddleware(
+  thunkMiddleware,
+  loggerMiddleware,
+  asyncDispatchMiddleware
+);
+
+const getMiddleware = () => {
+    if (process.env.NODE_ENV === "production") {
+      return ProdMiddlewares;
+    } else {
+      return DevMiddlewares;
+    }
+  };
+  
+const middlewares = composeWithDevTools(getMiddleware());
+  
+const store = createStore(rootReducer, middlewares);
 const persistor = persistStore(store);
 
 const StoreComponent = { persistor, store };
