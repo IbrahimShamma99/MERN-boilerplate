e720f374ebbfcf979c8782efb83703126f27ef15
ibrahimshamma99 <i.abushammah@gmail.com>
2020-06-05 19:55:52 +0300

    🐛 FIX:

---
+++ client/src/Templates/Base/Store/base.actions.js
@@ -0,0 +1,5 @@
+export const REFRESH = () => "REFRESH_STATE";
+export const MODIFY = "USER_EDIT";
+export const CREATE = () => "USER_LOGIN";
+export const FETCH_PROFILE = () => "FETCH_PROFILE";
+export const UPDATE = () => "USER_UPDATE";
---
+++ client/src/Templates/Base/Store/base.constants.js
@@ -0,0 +1,3 @@
+const BasicInfoInitState = {};
+
+export { BasicInfoInitState };
