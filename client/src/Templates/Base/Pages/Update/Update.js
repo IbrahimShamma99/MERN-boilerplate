0e117e4b9f1bef64c054aa33f7854394bda4bb1f
ibrahimshamma99 <i.abushammah@gmail.com>
2020-06-05 19:56:05 +0300

    👌 IMPROVE:

--- client/src/Templates/Base/Pages/Update/Update.js
+++ client/src/Templates/Base/Pages/Update/Update.js
@@ -1,9 +1,4 @@
 import React from "react";
-import "./Update.css";
-import * as actionTypes from "../../Store/user.actions";
-import Button from "react-bootstrap/Button";
-import { uploadAvatar } from "../../Utils/api-auth";
-
 
 class Update extends React.Component {
   state = {};
